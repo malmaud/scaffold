dde39ed6a3664250d576a5dfa277c1cc9f6a8118
Jon Malmaud <malmaud@gmail.com>
2012-11-28 17:36:06 -0500

    Discrete sampler can now robustly handle log-inputs and annealing temperature.

--- util.py
+++ util.py
@@ -12,7 +12,7 @@ import joblib
 
 memory = joblib.Memory('./data', mmap_mode='r', verbose=1)
 
-def discrete_sample(w, n, rng=random, log_mode=False, temperature=1):
+def discrete_sample(w, n, rng=random, log_mode=False, temperature=None):
     """
     Sample from a general  discrete distribution.
 
@@ -21,18 +21,24 @@ def discrete_sample(w, n, rng=random, log_mode=False, temperature=1):
     :param rng: The random number generator to use (e.g. as returned by *random.RandomState)
     :param log_mode: If *True*, interpret *w* as the log of the true weights. If *False*, interpret *w* as the literal
     weights. Default *False*.
-    :param temperature: The soft-max annealing temperature. A temperature of 1 corresponds to no modification to *w*.
+    :param temperature: The soft-max annealing temperature. *None* indicates not to use soft-max. A temperature of 1 corresponds to no modification to *w*.
+
      In $lim w\to 0, returns $argmax(w)$. In $lim w\to\infty$, returns *Uniform(len(w))*
     :return: A list of *n* integers, corresponding to the indices of *w* that were chosen.
     """
     w = asarray(w, 'd')
+    softmax = temperature is not None
     if log_mode:
+        if softmax:
+            raise BaseException("softmax in logmode not implemented")
         c = logaddexp.accumulate(w)
-        c -= c[-1]/temperature
+        c -= c[-1]
         r = log(rng.rand(n))
         return searchsorted(c, r)
     else:
+        if softmax:
+            w = exp(w/temperature)
         c = cumsum(w)
-        c /= c[-1]/temperature
+        c /= c[-1]
         r = rng.rand(n)
         return searchsorted(c, r)
